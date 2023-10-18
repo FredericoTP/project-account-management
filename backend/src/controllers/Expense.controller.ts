43c6b6411f6ef808a86cced312e4522f9d424e52
Frederico Tavares <fredericotp@hotmail.com>
2023-10-18 12:00:13 -0300

    Feat: ExpenseController

---
+++ backend/src/controllers/Expense.controller.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from 'express';
+import ExpenseService from '../services/Expense.service';
+
+class ExpenseController {
+  constructor(private expenseService = new ExpenseService()) {}
+
+  public async findAll(_req: Request, res: Response): Promise<Response> {
+    const expenses = await this.expenseService.findAll();
+
+    return res.status(200).json(expenses);
+  }
+
+  public async create(req: Request, res: Response): Promise<Response> {
+    const { expense } = req.body;
+
+    const newExpense = this.expenseService.create(expense);
+
+    return res.status(201).json(newExpense);
+  }
+
+  public async update(req: Request, res: Response): Promise<Response> {
+    const { oldExpense, newExpense } = req.body;
+
+    await this.expenseService.update(oldExpense, newExpense);
+
+    return res.status(200).json({ message: 'Expense has been updated' });
+  }
+}
+
+export default ExpenseController;
