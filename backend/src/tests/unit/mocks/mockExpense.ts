9f723c1440de01848b84338dcf269b21863e14ac
Frederico Tavares <fredericotp@hotmail.com>
2023-10-25 11:04:07 -0300

    Test: Unit test ExpenseService

---
+++ backend/src/tests/unit/mocks/mockExpense.ts
@@ -0,0 +1,19 @@
+const allExpenses = [{
+  dataValues: [{
+    id: 1,
+    expense: 'Alimentação',
+  },
+  {
+    id: 2,
+    expense: 'Energia',
+  }],
+}];
+
+const oneExpense = {
+  dataValues: [{
+    id: 1,
+    expense: 'Alimentação',
+  }],
+};
+
+export { allExpenses, oneExpense };
