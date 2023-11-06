43f50a03e044df1c7af9a73b50bf5379f7fea979
Frederico Tavares <fredericotp@hotmail.com>
2023-11-06 13:44:34 -0300

    Feat: Página Login

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,6 +1,42 @@
+import { useContext } from 'react';
+import LoginContext from '../context/LoginContext';
+
 function Login() {
+  const {
+    emailInput, passwordInput, isBtnDisabled, handleClick,
+  } = useContext(LoginContext);
+  const NUMBER8 = 8;
+
   return (
-    <h1>Página de Login</h1>
+    <div>
+      <h2>Login</h2>
+      <input
+        type="email"
+        placeholder="Email"
+        value={emailInput.value}
+        onChange={emailInput.handleChange}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={passwordInput.value}
+        onChange={passwordInput.handleChange}
+      />
+      {
+        (passwordInput.value.length > 0 && passwordInput.value.length < NUMBER8) && (
+          <small>
+            Password deve conter no mínimo 8 caracteres
+          </small>
+        )
+      }
+      <button
+        type="button"
+        disabled={isBtnDisabled()}
+        onClick={handleClick}
+      >
+        Entrar
+      </button>
+    </div>
   );
 }
 
