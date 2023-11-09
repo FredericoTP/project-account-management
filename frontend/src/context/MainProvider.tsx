eac710b94bd64f06af0c2e46bc9af5713497b926
Frederico Tavares <fredericotp@hotmail.com>
2023-11-09 12:00:28 -0300

    Feat: Implementando Home page

--- frontend/src/context/MainProvider.tsx
+++ frontend/src/context/MainProvider.tsx
@@ -1,14 +1,61 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AxiosError } from 'axios';
 import MainContext from './MainContext';
+import { hasSession, decodeToken, getToken } from '../utils/auth/authentication';
+import api from '../utils/apíInstance';
 
 type MainProviderProps = {
   children: React.ReactNode;
 };
 
 function MainProvider({ children }: MainProviderProps) {
-  const valueMain = useMemo(() => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [alertMessage, setAlertMessage] = useState('');
+  const [invoice, setInvoice] = useState([]);
 
-  }, []);
+  const valueMain = useMemo(() => {
+    const checkLogin = () => {
+      const token = hasSession();
+
+      if (!token) {
+        alert('Você precisa estar logado para acessar esta página!');
+        navigate('/', { replace: true });
+      }
+    };
+
+    const userInfo = () => {
+      setUser(decodeToken());
+    };
+
+    const accountInvoices = async () => {
+      try {
+        setIsLoading(true);
+        const token = getToken();
+
+        const response = await api.get('/invoice', { headers: { Authorization: token } });
+        setInvoice(response.data);
+        setIsLoading(false);
+      } catch (err) {
+        if (err instanceof AxiosError) {
+          setAlertMessage(err.response?.data.message);
+        }
+        setIsLoading(false);
+      }
+    };
+
+    return {
+      checkLogin,
+      userInfo,
+      user,
+      accountInvoices,
+      isLoading,
+      alertMessage,
+      invoice,
+    };
+  }, [navigate, user, isLoading, alertMessage, invoice]);
 
   return (
     <MainContext.Provider value={valueMain}>
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,31 @@
+import { useContext, useEffect } from 'react';
+import MainContext from '../context/MainContext';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function Home() {
+  const {
+    checkLogin, userInfo, user, isLoading, invoice, accountInvoices,
+  } = useContext(MainContext);
+
+  useEffect(() => {
+    checkLogin();
+    userInfo();
+    accountInvoices();
+  }, []);
+
+  console.log(invoice);
+
+  if (isLoading) {
+    return (
+      <>
+        <Header />
+        <p>Loading...</p>
+        <Footer />
+      </>
+    );
+  }
+
   return (
     <>
       <Header />
